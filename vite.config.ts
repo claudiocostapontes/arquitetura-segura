e636b0468590861afc4049567f0f3f7d66bb12ab
Claudionor Pontes <claudionor.pontes@outlook.com>
2026-05-03 20:20:37 -0300

    atualizacao de codigo

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
         artigos: resolve(__dirname, 'artigos.html'),
         sobre: resolve(__dirname, 'sobre.html'),
         trilhas: resolve(__dirname, 'trilhas.html'),
-        ferramentas: resolve(__dirname, 'ferramentas.html')
+        ferramentas: resolve(__dirname, 'ferramentas.html'),
+        // Adicione esta linha abaixo para a nova página funcionar no deploy
+        cve: resolve(__dirname, 'cve-watch.html') 
       }
     }
   }
